8257aa5dd3f1ede536b714cf95599b88c90f9d16
Your Name <mailyscolasse@iMac.local>
2022-06-02 01:13:34 +0200

    add new things in css + change position and size element

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,8 @@
-console.info('Hello world');
+const box = document.querySelector('.box');
+let scrollLine =  document.querySelector('.scroll');
+
+box.addEventListener('wheel', (e)=> {
+      e.preventDefault();
+      box.scrollLeft += e.deltaY;
+      scrollLinr.style.width = box.scrollLeft / 2 + 'px';
+})
